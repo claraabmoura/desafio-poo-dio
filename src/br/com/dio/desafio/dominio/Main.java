deb430ca4feb71d9564f50b736b63c3f49df5cd1
claraabmoura <claraabmoura@gmail.com>
2025-09-10 20:01:14 -0300

    final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -1,29 +1,63 @@
-package br.com.dio.desafio.dominio;
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
 
-import java.sql.SQLOutput;
 import java.time.LocalDate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Curso curso1 = new Curso();
-    curso1.setTitulo("Curso java");
-    curso1.setDescricao(("descrição curso java"));
-    curso1.setCargaHoraria(8);
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
 
-    Curso curso2 = new Curso();
-    curso2.setTitulo("Curso java");
-    curso2.setDescricao(("descrição curso js"));
-    curso2.setCargaHoraria(4);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
 
-    Mentoria mentoria = new Mentoria();
-    mentoria.setTitulo("mentoria java");
-    mentoria.setDescricao("descrição mentoria java");
-    mentoria.setData(LocalDate.now());
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
 
-    System.out.println(mentoria);
-    System.out.println(curso1);
-    System.out.println(curso2);
+        /*System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devClara = new Dev();
+        devClara.setNome("Clara");
+        devClara.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila:" + devClara.getConteudosInscritos());
+        devClara.progredir();
+        devClara.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Camila:" + devClara.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila:" + devClara.getConteudosConcluidos());
+        System.out.println("XP:" + devClara.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
     }
+
 }
